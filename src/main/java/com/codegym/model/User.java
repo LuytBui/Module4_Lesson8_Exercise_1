13a210b0b40618394445361dd01a81c511a8b948
LuytBui <luytbq43@gmail.com>
2022-03-28 11:55:23 +0700

    first commit

---
+++ src/main/java/com/codegym/model/User.java
@@ -0,0 +1,44 @@
+package com.codegym.model;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+
+public class User {
+
+    @NotEmpty
+    @Size(min = 2, max = 30, message = "Ten phai dai 2-30 ky tu")
+    private String name;
+
+    @Min(value = 18, message = "Tuoi phai lon hon 18")
+    private int age;
+
+    public User() {
+    }
+
+    public User(@NotEmpty @Size(min = 2, max = 30) String name, @Min(18) int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
